1f2a0e1855371b00a70355101c0102cbde17f81e
beaferreira <triszferreira@gmail.com>
2022-08-02 15:36:34 -0300

    criacao telas secundarias

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
